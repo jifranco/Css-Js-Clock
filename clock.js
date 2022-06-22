59fc9d84d4d088b44349fd6e9ff625f3c36ab891
Josue Franco <josuefranco32@gmail.com>
2022-06-22 11:17:51 -0700

    updated transition

--- clock.js
+++ clock.js
@@ -6,14 +6,30 @@ function setDate(){
 
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds/60)*360) +90;
+    if (secondsDegrees == 90) {
+        secondHand.classList.add("no-transition");
+      } else {
+        secondHand.classList.remove("no-transition");
+      }
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`
 
     const mints = now.getMinutes();
     const mintDegrees = ((mints/60)*360) +90;
+    if (minutesDegrees == 90) {
+        minuteHand.classList.add("no-transition");
+      } else {
+        minuteHand.classList.remove("no-transition");
+      }
     mintHand.style.transform =  `rotate(${mintDegrees}deg)`
 
     const hour = now.getHours();
     const hourDegrees = ((hour/12)*360)+90;
+    if (hoursDegrees == 90) {
+        hourHand.classList.add("no-transition");
+      } else {
+        hourHand.classList.remove("no-transition");
+      }
     hourHand.style.transform =  `rotate(${hourDegrees}deg)`
 }
+
 setInterval(setDate,1000);
